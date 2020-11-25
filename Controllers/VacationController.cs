e849fa2d90f211ac7ad28fadf2c498b7970dca22
Fady El Gawly <fgawly@gmail.com>
2020-11-25 22:20:28 +0200

    Vacations added, Arabic translation of  components

--- Controllers/VacationController.cs
+++ Controllers/VacationController.cs
@@ -7,8 +7,33 @@ using MarinaHR.Data;
 
 namespace MarinaHR.Controllers
 {
-    public class VacationController
+    //[Authorize]
+    public class VacationController : Controller
     {
-        
+        private readonly ApplicationDbContext context;
+
+        public VacationController(ApplicationDbContext _context)
+        {
+            context = _context;
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Vacation vacation)
+        {
+            context.Vacations.Add(vacation);
+            context.SaveChanges();
+            return View("Index", context.Vacations.ToList());
+        }
+
+        public ActionResult Index()
+        {
+            var data = context.Vacations.ToList();
+            return View(data);
+        }
     }
 }
--- Models/Vacation.cs
+++ Models/Vacation.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace MarinaHR.Models
 {
     public class Vacation
